c43d97fd3b751e60cd08ffd77e0df0950904b404
avv <akshatvijayvergiya64@gmail.com>
2025-08-23 22:53:42 +0530

    password strength checker

--- src/routes/app/auth.js
+++ src/routes/app/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { userSignUp } = require("../../controllers/app/auth");
+const { userSignUp, checkPasswordStrength } = require("../../controllers/app/auth");
 const { getUserData } = require("../../controllers/app/userData");
 
 const router = express.Router();
 
 router.post("/register", userSignUp);
 router.get("/get-user/:id", getUserData);
+router.post("/check-password-strength", checkPasswordStrength)
 
 module.exports = router;
