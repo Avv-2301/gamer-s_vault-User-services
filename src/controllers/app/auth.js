66f14a0298fbba80b14f1e438481def2f70060ff
avv <akshatvijayvergiya64@gmail.com>
2025-09-04 17:52:53 +0530

    login controller

--- src/controllers/app/auth.js
+++ src/controllers/app/auth.js
@@ -5,9 +5,14 @@ const User = require("../../models/auth");
 const {
   userSignUpValidation,
   passwordFieldValidation,
+  loginValidation,
 } = require("../../services/Validation");
 const bcrypt = require("bcrypt");
 const zxcvbn = require("zxcvbn");
+const UserLoginHistory = require("../../models/loginHistory");
+const { issueToken } = require("../../services/userJwt");
+const axios = require('axios');
+const ip = require('ip');
 
 module.exports = {
   /**
@@ -71,10 +76,10 @@ module.exports = {
               name: requestParams?.name,
               email: requestParams?.email,
               password: Hash_Password,
-              verified: Constant.FLAGS.FALSE,
+              verified: Constant.FLAGS.TRUE,
               authType: Constant.AUTH_TYPE.DIRECT,
               role: Constant.ROLE.USER,
-              status: Constant.FLAGS.INACTIVE,
+              status: Constant.FLAGS.ACTIVE,
             };
 
             const userResponse = await User.create(userObj); //user created
@@ -118,6 +123,157 @@ module.exports = {
    */
   login: async (req, res) => {
     try {
+      const requestParams = req.body;
+
+      if (!requestParams?.email || !requestParams?.password) {
+        return Response.errorResponseData(
+          res,
+          "All Fields Required",
+          Constant.STATUS_CODES.NO_CONTENT
+        );
+      }
+
+      const rememberMe = requestParams?.rememberMe;
+      // console.log(rememberMe, "REMEMBER");
+
+      loginValidation(requestParams, res, async (validate) => {
+        if (validate) {
+          const user = await User.findOne({ email: requestParams?.email });
+          // console.log(user, "USER DATAAA");
+
+          let system_ip = ip.address(); //system ip address
+
+          //get browser ip address
+          let browser_ip = await axios.get(
+            "https://api.ipify.org/?format=json"
+          );
+
+          if (user && user?.role === Constant.ROLE.USER) {
+            if (user && user?.verified !== null) {
+              if (user && user?.status === Constant.FLAGS.ACTIVE) {
+                const comparePassword = await bcrypt.compare(
+                  requestParams?.password,
+                  user.password
+                );
+                if (comparePassword) {
+                  const expiresIn = rememberMe ? 60 * 60 * 24 * 15 : 60 * 60;
+                  console.log(expiresIn)
+                  const userExpTime = Math.floor(Date.now() / 1000) + expiresIn;
+                  console.log(userExpTime)
+
+                  const payload = {
+                    id: user?._id,
+                    role: user?.role,
+                    expiry: userExpTime,
+                  };
+
+                  const token = issueToken(payload);
+
+                  const meta = { token };
+                  let tokenUpdate = {};
+
+                  tokenUpdate = {
+                    $set: {
+                      last_login: new Date(),
+                      token: token,
+                      tokenExpiresAt: userExpTime,
+                      "ip_address.system_ip": system_ip,
+                      "ip_address.browser_ip": browser_ip?.data?.ip,
+                    },
+                  };
+
+                  let loginHistory = await UserLoginHistory.findOne({
+                    userId: user?._id,
+                  });
+                  let loginHistoryObject = {};
+
+                  if (loginHistory) {
+                    loginHistoryObject = {
+                      loginDetails: {
+                        ip_address: {
+                          system_ip: system_ip,
+                          browser_ip: browser_ip?.data?.ip,
+                        },
+                        last_login: new Date(),
+                      },
+                    };
+
+                    let update = {};
+                    if (loginHistory?.loginDetails?.length !== 100) {
+                      tenRecords = loginHistoryObject;
+                      update = {
+                        $push: {
+                          loginDetails: loginHistoryObject?.loginDetails,
+                        },
+                      };
+                    } else {
+                      update = {
+                        $set: {
+                          loginDetails: [],
+                        },
+                      };
+                    }
+
+                    await UserLoginHistory.updateOne(
+                      { userId: user._id },
+                      update
+                    );
+                  } else {
+                    loginHistoryObject = {
+                      userId: user._id,
+                      loginDetails: {
+                        ip_address: {
+                          system_ip: system_ip,
+                          browser_ip: browser_ip?.data?.ip,
+                        },
+                        last_login: new Date(),
+                      },
+                    };
+
+                    const history = await UserLoginHistory.create(
+                      loginHistoryObject
+                    );
+                    tokenUpdate.$set.loginHistory = history?._id;
+                  }
+                  await User.updateOne({ _id: user?._id }, tokenUpdate);
+
+                  return Response.successResponseData(
+                    res,
+                    user,
+                    Constant.STATUS_CODES.SUCCESS,
+                    "Login Successfull",
+                    meta
+                  );
+                } else {
+                  return Response.validationErrorResponseData(
+                    res,
+                    "Password Not Correct",
+                    Constant.STATUS_CODES.UNAUTHORIZED
+                  );
+                }
+              } else {
+                return Response.errorResponseWithoutData(
+                  res,
+                  "User is InActive",
+                  Constant.STATUS_CODES.BAD_REQUEST
+                );
+              }
+            } else {
+              return Response.errorResponseWithoutData(
+                res,
+                "User is not Verified",
+                Constant?.STATUS_CODES.BAD_REQUEST
+              );
+            }
+          } else {
+            return Response.errorResponseWithoutData(
+              res,
+              "Access denied",
+              Constant.STATUS_CODES.BAD_REQUEST
+            );
+          }
+        }
+      });
     } catch (error) {
       console.log(error);
       return Response.errorResponseData(
